87f506af9455b314f8e0aa5689da82a4413eea79
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-21 11:15:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-  array.length == 2
-    "#{array[0]} and #{array[1]}"
-  array.join
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
